525a2a96fc23e314a128d47dfaf46c60ed7ca78b
Jeff Eiden <jeff.m.eiden@gmail.com>
2016-09-13 10:23:13 -0700

    Check for config before starting to attempt to authenticate with Google

--- tutorial.js
+++ tutorial.js
@@ -1,13 +1,14 @@
-/* CONFIGURATION */
-var config = {
-	gcpProjectId: '[YOUR PROJECT ID HERE]',
-	gcpPubSubSubscriptionName: '[YOUR SUBSCRIPTION NAME HERE]',
-	gcpServiceAccountKeyFilePath: './gcp_private_key.json'
-}
-/* END CONFIGURATION */
-
 var colors = require('colors');
 var util = require('util');
+
+/* CONFIGURATION */
+var config = {
+	gcpProjectId: '',
+	gcpPubSubSubscriptionName: '',
+	gcpServiceAccountKeyFilePath: './gcp_private_key.json'
+}
+_checkConfig();
+/* END CONFIGURATION */
 console.log(colors.magenta('Authenticating with Google Cloud...'))
 var gcloud = require('google-cloud')({
     projectId: config.gcpProjectId,
@@ -38,6 +39,27 @@ function storeEvent(message) {
 
 };
 
+subscription.on('message', function(message) {
+	console.log(colors.cyan('Particle event received from Pub/Sub!\r\n'), _createEventObjectForStorage(message, true));
+	// Called every time a message is received.
+	// message.id = ID used to acknowledge its receival.
+	// message.data = Contents of the message.
+	// message.attributes = Attributes of the message.
+	storeEvent(message);
+	message.ack();
+});
+
+function _checkConfig() {
+	if(config.gcpProjectId === ''  || !config.gcpProjectId) {
+		console.log(colors.red('You must set your Google Cloud Platform project ID in tutorial.js'));
+		process.exit(1);
+	}
+	if(config.gcpPubSubSubscriptionName === '' || !config.gcpPubSubSubscriptionName) {
+		console.log(colors.red('You must set your Google Cloud Pub/Sub subscription name in tutorial.js'));
+		process.exit(1);
+	}
+};
+
 function _createEventObjectForStorage(message, log) {
 	var obj = {
 		gc_pub_sub_id: message.id,
@@ -53,15 +75,5 @@ function _createEventObjectForStorage(message, log) {
 		return obj;
 	}
 };
-
-subscription.on('message', function(message) {
-	console.log(colors.cyan('Particle event received from Pub/Sub!\r\n'), _createEventObjectForStorage(message, true));
-	// Called every time a message is received.
-	// message.id = ID used to acknowledge its receival.
-	// message.data = Contents of the message.
-	// message.attributes = Attributes of the message.
-	storeEvent(message);
-	message.ack();
-});
 
 
